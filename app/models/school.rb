75fb95ac627c5718fae442f6bc2c8322e32212c1
Todd Tyree <tatyree@gmail.com>
2020-03-14 21:22:28 +0000

    WIP: Individual importer working

--- app/models/school.rb
+++ app/models/school.rb
@@ -46,12 +46,28 @@ class School < ApplicationRecord
     ['None', 'Does not apply', nil].exclude?(self.gias_data['religious_character'])
   end
 
+  def self.bigquery_load(bigquery: Bigquery.new)
+    dataset = bigquery.dataset(BIGQUERY_DATASET)
+    table_name = [BIGQUERY_TABLE_PREFIX, self.name.to_s.downcase.pluralize].join('_')
+    table = dataset.table(table_name)
+    table = dataset.create_table(table_name) do |schema|
+      bigquery_schema.each do |column_name, column_type|
+        schema.send(column_type, column_name)
+      end
+    end if table.nil?
+
+    find_in_batches(batch_size: 500) do |batch|
+      insert_response = table.insert batch.map(&:bigquery_data)
+      Rails.logger.info({ bigquery: table_name, insert_response: insert_response, records: batch.size }.to_json)
+    end
+  end
+
   def bigquery_export(bigquery: Bigquery.new)
     dataset = bigquery.dataset(BIGQUERY_DATASET)
     table_name = [BIGQUERY_TABLE_PREFIX, self.class.name.to_s.downcase.pluralize].join('_')
     table = dataset.table(table_name)
     table = dataset.create_table(table_name) do |schema|
-      bigquery_schema.each do |column_name, column_type|
+      self.class.bigquery_schema.each do |column_name, column_type|
         schema.send(column_type, column_name)
       end
     end if table.nil?
@@ -90,15 +106,19 @@ class School < ApplicationRecord
     gias = gias_data || self.class.where.not(gias_data: nil).first.gias_data
     gias.map do |key, value|
       data = no_gias_data ? nil : value
-      data = nil if data.empty?
+      data = nil if data.blank?
       data = Date.parse(data).to_s(:db) if data.is_a?(String) && data.match?(/^\d{2}\-\d{2}\-\d{4}/)
       data = data.to_i if data.is_a?(String) && data.match?(/^\d+$/)
-      @bigquery_data["data_#{key.chomp(')').gsub(/\W+/, '_')}"] = data
+      @bigquery_data[data_key_name(key)] = data
     end
     @bigquery_data
   end
 
-  def bigquery_schema
+  def data_key_name(key)
+    "data_#{key.chomp(')').gsub(/\W+/, '_').downcase}"
+  end
+
+  def self.bigquery_schema
     @bigquery_schema = {}
     convert_these_attributes = {
       'phase' => :string
@@ -122,23 +142,23 @@ class School < ApplicationRecord
       supporting_documents
     ].freeze
 
-    self.class.columns.map do |c|
+    columns.map do |c|
       next if drop_these_attributes.include?(c.name)
       @bigquery_schema[c.name] = convert_these_attributes[c.name] || convert_these_types[c.type] || c.type
     end.compact
 
-    if self.class.column_names.include?('geolocation')
+    if column_names.include?('geolocation')
       @bigquery_schema['geolocation_x'] = :float
       @bigquery_schema['geolocation_y'] = :float
     end
 
-    gias = gias_data || self.class.where.not(gias_data: nil).first.gias_data
+    gias_schema = where.not(gias_data: nil).first.gias_data
 
-    gias.map do |key, value|
+    gias_schema.sort_by{ |k, _| k }.map do |key, value|
       data_type = :string
       data_type = :date if key.match(/date/i)
-      data_type = :integer if value.match(/^\d+$/)
-      @bigquery_schema["data_#{key.chomp(')').gsub(/\W+/, '_')}"] = data_type
+      data_type = :integer if value.match(/^\d+$/) && !key.match(/diocese/i)
+      @bigquery_schema[data_key_name(key)] = data_type
     end
 
     @bigquery_schema
