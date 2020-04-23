aebfa9c7ad5c145af74a0f21f7c884cb90d7f8a6
alexbowen <alexbowen@Alexs-MBP.lan>
2020-04-23 09:39:54 +0100

    remove algolia api keys

--- app/frontend/src/search/client.js
+++ app/frontend/src/search/client.js
@@ -1,4 +1,4 @@
-export const search = algoliasearch('QM2YE0HRBW', '20b88d28047d5e3d60437993ad3d9c50')
+export const search = algoliasearch('', '')
 
 // TODO remove all this global scope
 export const client = indexName => instantsearch({
